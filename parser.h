ff1fa6af69ef5a20b05e908d1f4f4b73fe5f9613
whwilder <whwhilder@email.arizona.edu>
2016-07-11 17:34:44 -0700

    Did some refactoring, mostly to get code to compile with g++

---
+++ parser.h
@@ -0,0 +1,21 @@
+#define CORE_SIZE 8000
+#define MAX_CYCLES 8000
+#define MAX_PROCESSES 8000
+
+typedef struct InstrNode {
+   int opcode;
+   int arg1;
+   int arg2;
+   int mode_a;
+   int mode_b;
+   int modifier;
+   struct InstrNode *next;
+} InstrNode;
+
+enum opCode {DAT, MOV, ADD, SUB, MUL, DIV, MOD, JMP, JMZ, DJN, SPL, CMP, SEQ, SNE, SLT, LDP,   STP, NOP};
+enum addr_mode {IMM, DIRECT, INDIR_A, INDIR_B, PREDEC_A, PREDEC_B, POSTINC_A, POSTINC_B};
+enum modifier  {A, B, AB, BA, F, X, I, NONE};
+
+typedef enum opCode OpCode;
+typedef enum addr_mode addr_mode;
+typedef enum modifier modifier;
